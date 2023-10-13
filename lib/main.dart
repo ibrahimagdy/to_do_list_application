598fc24530fd94d3f939fcf4ef9868a05a7ee0ed
Ibrahim <iibbrraahhimm00@gmai;.com>
2023-10-13 21:11:57 +0300

    bottom sheet widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:to_do_application/core/theme/application_theme.dart';
 import 'package:to_do_application/layout/home_layout.dart';
 import 'package:to_do_application/moduls/splash_screen/splash_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MyApplication(),
   );
---
+++ lib/moduls/home_screen/widgets/task_item_widget.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class TaskItemWidget extends StatelessWidget {
+  const TaskItemWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: const LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: [
+            Color(0xFFEC4B4B),
+            Color(0xFFEC4B4B),
+            Color(0xFFE8DD1B),
+            Color(0xFFE8DD1B),
+          ],
+        ),
+      ),
+      child: Slidable(
+        startActionPane: ActionPane(
+          extentRatio: 0.2,
+          motion: const DrawerMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (context) {},
+              borderRadius: BorderRadius.circular(15),
+              backgroundColor: const Color(0xFFEC4B4B),
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Delete',
+            ),
+          ],
+        ),
+        endActionPane: ActionPane(
+          extentRatio: 0.2,
+          motion: const DrawerMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (context) {},
+              borderRadius: BorderRadius.circular(15),
+              backgroundColor: const Color(0xFFE8DD1B),
+              foregroundColor: Colors.white,
+              icon: Icons.edit,
+              label: 'Edit',
+            ),
+          ],
+        ),
+        child: Container(
+          width: 400,
+          height: 125,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.white,
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 5,
+                height: 80,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: theme.primaryColor,
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Play basket ball",
+                    style: theme.textTheme.bodyLarge,
+                  ),
+                  Text(
+                    "description",
+                    style: theme.textTheme.bodyMedium,
+                  ),
+                  Row(
+                    children: [
+                      const Icon(
+                        CupertinoIcons.clock,
+                        size: 20,
+                      ),
+                      const SizedBox(width: 5),
+                      Text(
+                        "10:30 AM",
+                        style: theme.textTheme.bodySmall,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                width: 70,
+                height: 37,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: theme.primaryColor,
+                ),
+                child: Image.asset("assets/images/check_icon.png"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
